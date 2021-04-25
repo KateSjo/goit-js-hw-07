6f1b144f99cd8e3aaedd3f5b46e55d37b9d82361
KateSjo <74876776+KateSjo@users.noreply.github.com>
2021-04-25 16:56:41 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,18 +1,14 @@
 const inputWord = document.querySelector('#validation-input');
+
 inputWord.addEventListener('blur', onInputCheck)
 
-function onValidationInput() {
-    inputWord.classList.add('valid');
-}
-
-function onInvalidationInput() {
-    inputWord.classList.toggle('invalid');
-}
-
-function onInputCheck(event) {
-    if (event.currentTarget.value.length === 6) {
-        onValidationInput();
-    };
-    onInvalidationInput();
-    console.log(event.currentTarget.value.length);
+function onInputCheck(event){
+    if(event.currentTarget.value.length === Number(inputWord.dataset.length)) {
+        inputWord.classList.add('valid')
+        inputWord.classList.remove('invalid')
+        return;
+    }
+    inputWord.classList.add('invalid')
+    inputWord.classList.remove('valid')
+    return;
 }
